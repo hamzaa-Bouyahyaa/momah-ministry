9a162693e8138d3a34e3d3636831152c2b736228
hamzaa-bouyahyaa <hamzabouyahya198@gmail.com>
2026-02-18 21:58:29 +0100

    adding box shadow for the background

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,23 +3,25 @@ import loginImage from "@/assets/login-momah.svg";
 
 function AuthLayout() {
   return (
-    <>
-      {/* Form panel */}
-      <div className="flex w-full flex-col items-center justify-center px-6 py-12 lg:w-1/2">
-        <div className="w-full max-w-md">
-          <Outlet />
+    <div className="flex min-h-svh items-center justify-center p-4">
+      <div className="flex w-full max-w-[1100px] overflow-hidden rounded-3xl bg-background shadow-[0_-20px_60px_-10px_rgba(0,0,0,0.1)]">
+        {/* Form panel */}
+        <div className="flex w-full flex-col items-center justify-center px-6 py-12 lg:w-1/2">
+          <div className="w-full max-w-md">
+            <Outlet />
+          </div>
+        </div>
+
+        {/* Branding panel */}
+        <div className="hidden lg:block lg:w-1/2">
+          <img
+            src={loginImage}
+            alt="المنصة الموحدة للمكتب التنفيذي"
+            className="h-full w-full object-cover"
+          />
         </div>
       </div>
-
-      {/* Branding panel */}
-      <div className="hidden lg:block lg:w-1/2">
-        <img
-          src={loginImage}
-          alt="المنصة الموحدة للمكتب التنفيذي"
-          className="h-full w-full object-cover"
-        />
-      </div>
-    </>
+    </div>
   );
 }
 
